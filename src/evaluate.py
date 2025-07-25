fd7611344119aae7d809dd0693e020aefc170e37
hoangducanh1865 <hoangducanh.1865@gmail.com>
2025-07-26 03:13:27 +0700

    “Update”

--- src/evaluate.py
+++ src/evaluate.py
@@ -7,4 +7,4 @@ def evaluate_model(dataset, trainer, top_words, train_theta):
     test_theta = trainer.test(dataset.test_data)
     clustering_results = eva._clustering(test_theta, dataset.test_labels)
     classification_results = eva._cls(train_theta, test_theta, dataset.train_labels, dataset.test_labels)
-    return TD, TC, clustering_results, classification_results
+    return TD, TC, clustering_results, classification_results, test_theta
